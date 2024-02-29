4cc075edbf47c841892796cd2a266ad98b508072
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2024-02-29 17:00:27 -0500

    chore: rm-ed unnecessary info

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,3 @@
-/*********************************************************************************
-*  Assignment 6
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
-* 
-*  Name:       Vladyslav Savchyn 
-*  Date:       12.07.2023
-*  Vercel App: https://next-museum-ruby.vercel.app/
-*
-********************************************************************************/
-
-
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
